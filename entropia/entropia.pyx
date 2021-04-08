e294586cb913cec9480de7a1cd4d68ad0a6320ed
jbarberia <jbarberia@frba.utn.edu.ar>
2021-04-08 09:08:56 -0300

    Docstrings

--- entropia/entropia.pyx
+++ entropia/entropia.pyx
@@ -8,19 +8,19 @@ cdef double mean(list x):
 
 
 cdef double distance(list arr, double x):
-    """Distancia media al cuadrado"""
+    """Desvio del arr con respecto al valor x (Distancia cuadratica media)"""
     return mean([(y-x)**2 for y in arr])
 
 
 def argsort(list arr):
-    """Ordena una lista y devuelve sus indices"""
+    """Devuelve los indices que resultan al ordenar arr"""
     to_sort = [(i, v) for i, v in enumerate(arr)]
     sort = sorted(to_sort, key=lambda x: x[1])
     return [i for i, _ in sort]
 
 
 def ordinal_patterns(list x, int m=3, int t=1):
-    """Devuelve un vector de probabilidades de cada patron posible"""
+    """Devuelve un vector de probabilidades de cada patron posible (Omite patrones faltantes)"""
     partition = [x[i:i+1+(m-1)*t] for i in range(0, len(x)-(m-1)*t, t)]
 
     permutation = map(lambda x: tuple(argsort(x)), partition)
@@ -55,20 +55,49 @@ def weight_patterns(list x, int m=3, int t=1):
 
 
 def weight_entropy(list x, int m=3, int t=1):
+    """Entropia ponderada (No Normalizada)
+    
+    arguments:
+    x -- [list]: Senal a analizar
+    m -- [int]: Dimension de embedding
+    t -- [int]: Delay de muestreo
+    """
     y = weight_patterns(x, m, t)
     return - sum(x * log(x) for x in y)
 
 
 def bandt_and_pompe(list x, int m=3, int t=1):
+    """Entropia de Bandt & Pompe (Shannon, Permutacion)
+    
+    arguments:
+    x -- [list]: Senal a analizar
+    m -- [int]: Dimension de embedding
+    t -- [int]: Delay de muestreo
+    """
     y = ordinal_patterns(x, m, t)
     return - sum(x * log2(x) for x in y)
 
 
 def bandt_and_pompe_normal(list x, int m=3, int t=1):
+    """Entropia de Bandt & Pompe (Normalizada)
+    
+    arguments:
+    x -- [list]: Senal a analizar
+    m -- [int]: Dimension de embedding
+    t -- [int]: Delay de muestreo
+    """
     return bandt_and_pompe(x, m, t) / log2(factorial(m))
 
 
 def renyi_entropy(list x, int q=1, int m=3, int t=1):
+    """Entropia de renyi
+    
+    arguments:
+    x -- [list]: Senal a analizar
+    q -- [int]: Exponente
+    m -- [int]: Dimension de embedding
+    t -- [int]: Delay de muestreo
+    """
     if q == 1:
         return bandt_and_pompe(x, m, t)
     y = ordinal_patterns(x, m, t)
@@ -76,11 +105,22 @@ def renyi_entropy(list x, int q=1, int m=3, int t=1):
 
 
 def complexity_entropy(list x, int m=3, int t=1):
+    """ Devuelve una tupla (e, jsd, cjs) con los siguientes valores:
+    e: Entropia de Bandt & Pompe Normalizada
+    jsd: Divergencia de Jansen-Shannon
+    cjs: Complejidad basada en la divergencia de Jansen-Shannon
+    
+    arguments:
+    x -- [list]: Senal a analizar
+    q -- [int]: Exponente
+    m -- [int]: Dimension de embedding
+    t -- [int]: Delay de muestreo
+    """
     n = factorial(m)
     op = ordinal_patterns(x, m, t)
     h1 = -sum(y * log(y) for y in op)
 
-    Q = - 1 / ((0.5+0.5/n)*log(0.5+(0.5/n)) + (0.5/n)*log(0.5/n)*(n-1) + 0.5*log(n))
+    cdef double Q = - 1 / ((0.5+0.5/n)*log(0.5+(0.5/n)) + (0.5/n)*log(0.5/n)*(n-1) + 0.5*log(n))
     
     op2 = [(0.5*y + 0.5/n) for y in op] + ([0.5/n] * (n - len(op))) # Concatenacion de listas
     h2 = -sum(x * log(x) for x in op2)
